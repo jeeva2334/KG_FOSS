3a04019fd9a6865d2e9627acf4d02ccd301f3cea
Manjusridharan <msri10792@gmail.com>
2023-03-18 12:27:55 +0530

    function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+let result = addNumbers(2, 3) // result will be 5
+console.log(result);
+let x = 5;
+let y = 7;
+ result = addNumbers(x, y); // result will be 12
+console.log(result);
